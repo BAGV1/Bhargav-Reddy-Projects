da4c7d99df68f55e5c450cf13e7b5f119f71606d
Bhargav <bhargav.reddy2003@gmail.com>
2026-05-17 00:36:44 -0500

    Update analyzer.py

--- Network-Anomaly-Detector/analyzer.py
+++ Network-Anomaly-Detector/analyzer.py
@@ -5,7 +5,17 @@ calculate the packet Z-Score
 import numpy as np
 from scipy.stats import zscore
 
-def calculate_anomalies(data_list):
-    data_list = np.array(data_list)
-    scores = zscore(data_list)
-    return scores
+"""
+Find Size Distribution, Time Intervals
+Computes Z-Scores for both to find anomalies
+"""
+def calculate_anomalies(sizes, times):
+    sizes = np.array(sizes)
+
+    time_intervals = np.diff(times)
+    sizes = sizes[1:]
+
+    scores = zscore(sizes)
+    time_scores = zscore(time_intervals)
+
+    return scores, time_scores
