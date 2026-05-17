18a72c57c69f511696d10781ddff7630a7e704f0
Bhargav <bhargav.reddy2003@gmail.com>
2026-05-17 00:36:55 -0500

    Update reporter.py

--- Network-Anomaly-Detector/reporter.py
+++ Network-Anomaly-Detector/reporter.py
@@ -1,10 +1,18 @@
 import pandas as pd
 
-def create_report(sizes, scores):
-    data_mapping = {'Sizes': sizes, 'Z-Score': scores}
+def create_report(sizes, size_scores, time_scores):
+    data_mapping = {'Sizes': sizes,
+                    'Size Z-Score': size_scores,
+                    'Time Z-Score': time_scores
+                    }
+
     report_table = pd.DataFrame(data_mapping)
+
     # Taking absolute value is an easy way to
     # get both high and low anomalies
-    absolute_scores = report_table['Z-Score'].abs()
-    anomalies = report_table[absolute_scores > 2.5]
+    size_anomalies = report_table['Size Z-Score'].abs() > 2.5
+    time_anomalies = report_table['Time Z-Score'].abs() > 2.5
+
+    anomalies = report_table[size_anomalies | time_anomalies]
+
     return anomalies
