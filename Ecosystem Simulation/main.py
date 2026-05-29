9f77d19f7f0ad6b1abd5476cf070283447b8046c
Bhargav <bhargav.reddy2003@gmail.com>
2026-05-29 00:21:46 -0500

    Update ecosystem simulation with animal management

--- Ecosystem Simulation/main.py
+++ Ecosystem Simulation/main.py
@@ -1,12 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from animal import AnimalManager, Rabbit, Lion
 
 CONFIG = {"biome": "savannah",
           "tick_speed": 0.1,
           "plant_density": 0.3,
           "water_density": 0.1,
-          "map_width": 50,
-          "map_height": 50,
+          "map_width": 100,
+          "map_height": 100,
           }
 
 class Biome:
@@ -30,23 +31,37 @@ class Grid:
 
 
 class Renderer:
-    def __init__(self, grid, biome):
+    def __init__(self, grid, biome, manager):
         self.grid = grid
         self.biome = biome
+        self.manager = manager
 
         self.fig, self.ax = plt.subplots()
-        self.im = self.ax.imshow(self.grid.cells, cmap='YlGn')
+        self.cmap = plt.matplotlib.colors.ListedColormap(["#C2B280", "#4a7c59", "#4a90d9"])
+        self.im = self.ax.imshow(self.grid.cells, cmap=self.cmap, vmin=0, vmax=2)
 
     def update(self):
         self.im.set_data(self.grid.cells)
-        plt.pause(0.1)
 
+        self.ax.clear()
+        self.ax.imshow(self.grid.cells, cmap=self.cmap, vmin=0, vmax=2)
+        for animal in self.manager.animals:
+            self.ax.plot(animal.x, animal.y, 'o', color=animal.color, markersize=1.25)
+
+        plt.pause(1)
 
 if __name__ == "__main__":
     grid = Grid(CONFIG["map_width"], CONFIG["map_height"])
     biome = Biome("savannah", "yellow", CONFIG["plant_density"], CONFIG["water_density"])
     biome.generate_terrain(grid)
-    renderer = Renderer(grid, biome)
+
+    manager = AnimalManager(grid)
+    manager.add_animal(Rabbit(10, 10))
+    manager.add_animal(Rabbit(20, 15))
+    manager.add_animal(Lion(30, 30))
+
+    renderer = Renderer(grid, biome, manager)
 
     while True:
         renderer.update()
+        manager.update()
